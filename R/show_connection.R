9e9d49f8e8a3f2dfea65e29ead50532ea58aa339
Holtz Yan <yan.holtz.data@gmail.com>
2018-03-13 18:15:25 +1000

    Add genetic map example

--- R/show_connection.R
+++ R/show_connection.R
@@ -7,6 +7,8 @@
 #' of the item, V2 its position
 #' @param tokeep a vector of item to keep on the graphic. All item not present in this list
 #' will be discarded. By default, all markers are kept.
+#' @param showName wether or not you want to show item names on the graphic.If you're working
+#' with a huge number of item, you probably don't want to show them
 #' @keywords plot connection list
 #' @author Lisa de Matteo
 #' @export
@@ -29,7 +31,7 @@
 #' show_connection(o1, o2, tokeep=res$LCS)
 
 # A function that plot the relationship between 2 lists:
-show_connection <- function(o1, o2, tokeep=NA){
+show_connection <- function(o1, o2, tokeep=NA, showName=TRUE){
   
   # Prepare the data: merge the 2 lists and make a 'long' or 'tidy' format:
   don <- merge(o1, o2, by.x="V1", by.y="V1", all=T) %>%
@@ -37,20 +39,34 @@ show_connection <- function(o1, o2, tokeep=NA){
     gather(ord, position, -1) %>%
     na.omit()
   
-  # It is possible to keep only a sample of the elements:
+  # Keep only a sample of the elements:
   if( all(is.na(tokeep))==FALSE ){ don <- don %>% filter(name %in% tokeep) }
   
   # Make the plot
-  ggplot( don, aes(x=ord, y=position, label=name, group=name, color=name)) +
+  p <- ggplot( don, aes(x=ord, y=position, label=name, group=name, color=name)) +
     geom_segment( x=1, xend=1, y=-max(o1$V2), yend=0, color="black", size=0.3) +
     geom_segment( x=2, xend=2, y=-max(o2$V2), yend=0, color="black", size=0.3) +
     geom_point() +
-    geom_text_repel( data=don %>% filter(ord=="order1"), direction = "y" , segment.color = "grey", box.padding = 0.001, color="black", xlim=c(0,.95)) +
-    geom_text_repel( data=don %>% filter(ord=="order2"), direction = "y" , segment.color = "grey", box.padding = 0.001, color="black", xlim=c(2.05,NA)) +
     geom_line() +
     theme_void() +
     theme(legend.position="none") +
-    #scale_y_reverse() +
     xlab("") +
     ylim( max(c(o1$V2, o2$V2)), 0)
+
+  # Add item names
+  if(showName==TRUE){
+    p <- p +
+        geom_text_repel( data=don %>% filter(ord=="order1"), direction = "y" , segment.color = "grey", box.padding = 0.001, color="black", xlim=c(0,.95)) +
+        geom_text_repel( data=don %>% filter(ord=="order2"), direction = "y" , segment.color = "grey", box.padding = 0.001, color="black", xlim=c(2.05,NA))
+  }
+    
+  p
 }
+
+
+
+
+
+
+
+
